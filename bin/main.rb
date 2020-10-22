4da777beea90bf3240897495fe869462131b0a56
Ahmad Ramdani <ahmad.ramdani@imdglobalservices.com>
2020-10-22 16:32:18 +0700

    add comment to some line codes and fix some

--- bin/main.rb
+++ bin/main.rb
@@ -6,9 +6,9 @@ class Main
     loop do
       while (1)
         print("Search > ")
-        command = STDIN.gets.chomp
+        command = STDIN.gets.chomp #read user's input
         break if command == '/q'
-        Searchable.new(command).run
+        Searchable.new(command).run #calling search function and print the output
       end
       break
     end
--- bin/searchable.rb
+++ bin/searchable.rb
@@ -4,43 +4,45 @@ require 'table_print'
 
 class Searchable
   def initialize(param)
-    @params = str_to_array(param)
+    @params = str_to_array(param) #convert string to array
     base_path = File.join(File.dirname(File.expand_path(__FILE__)))
     file_path = File.join(base_path,'..','data.json')
-    @data = JSON.parse(File.read(file_path))
-    @exact_param = scan_exact_param(param)
-    @negative_param = scan_negative_param(param)
+    @data = JSON.parse(File.read(file_path)) #get data from JSON file
+    @exact_param = scan_exact_param(param) #get string contains exact param "string"
+    @negative_param = scan_negative_param(param) #get string contains negative param "--negative" > "negative"
   end
 
   def run
+    #print output to console using table print. it makes look pretty
     tp fulltextsearch, 'Name', { 'Type' => {:width => 60} }, { 'Designed by' => {:width => 100} }
   end
 
   def str_to_array(str)
-    param = str.downcase.split(/\s(?=(?:[^'"]|'[^']*'|"[^"]*")*$)/)
-    param = rm_double_quote(param)
+    param = str.downcase.split(/\s(?=(?:[^'"]|'[^']*'|"[^"]*")*$)/) #split by space and remove whitespaces
+    param = rm_double_quote(param) #remove double quotes with a slash
     return param
   end
 
   def scan_exact_param(str)
-    scanner = str.downcase.scan(/".*?"/)
-    scanner = rm_double_quote(scanner)
+    scanner = str.downcase.scan(/".*?"/) #get string covered quotations e.g. 'hello "world"'
+    scanner = rm_double_quote(scanner) #remove double quotes with slash
     return scanner
   end
 
   def scan_negative_param(str)
-    scanner = str.downcase.scan(/\--\w+/)
-    scanner = rm_double_quote(scanner)
+    scanner = str.downcase.scan(/\--\w+/) #get string who has a special character in the beginning e.g. '--array'
+    scanner = rm_double_quote(scanner) #remove double quotes with a slash
     return scanner
   end
 
   def rm_double_quote(arr)
+    #remove double quotes with slash "\"here\"" to "here"
     return arr.map {|s| s.gsub(/(^ +)|( +$)|(^["']+)|(["']+$)/,'')}
   end
 
   def fulltextsearch
-    return by_name unless by_name.empty? #priority search
-    return by_fields #secondary search
+    return by_name unless by_name.empty? #assuming search by name as priority
+    return by_fields #then search custom if by name is empty
   end
 
   def by_name
@@ -48,14 +50,14 @@ class Searchable
   end
 
   def by_fields
-    data = by_exact unless @exact_param.empty?
-    data = by_spesifict if !data || data.empty?
-    data = by_global if !data || data.empty?
-    data = by_negative(data) unless @negative_param.empty?
+    data = by_exact unless @exact_param.empty? #search if param is the exact match type
+    data = by_spesific if !data || data.empty? #search by specific type of exact have no data
+    data = by_global if !data || data.empty? #search by global type if by_spesific have no data
+    data = by_negative(data) unless @negative_param.empty? #combine matched search with negative type
     return data
   end
 
-  def by_spesifict
+  def by_spesific
     return @data.select{ |key| Regexp.union(@params) =~ key['Type'].downcase &&
       Regexp.union(@params) =~ key['Designed by'].downcase }
   end
